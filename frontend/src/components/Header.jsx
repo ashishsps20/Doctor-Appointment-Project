6e00214c348fe0e109e2d1d3798f2ac1d6ed22f1
ashishsps20 <ashishsps20@gmail.com>
2025-04-13 17:26:51 +0530

    initializing project

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div> 
+      {/* -----left Side-----*/}
+      <div>
+
+      </div>
+
+      {/* -----Right Side-----*/}
+      <div>
+
+      </div>
+    </div>
+  )
+}
+
+export default Header
