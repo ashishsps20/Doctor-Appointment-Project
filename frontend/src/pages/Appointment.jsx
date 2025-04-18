d16776b54f223af8d868e29bd25dd767d5b71f36
ashishsps20 <ashishsps20@gmail.com>
2025-04-18 15:41:59 +0530

    Adding my Appointment

--- frontend/src/pages/Appointment.jsx
+++ frontend/src/pages/Appointment.jsx
@@ -1,8 +1,40 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { AppContext } from '../context/AppContext';
+import { assets } from '../assets/assets_frontend/assets';
 
 const Appointment = () => {
-  return (
+
+  const { docId } = useParams();
+  const { doctors } = useContext(AppContext);
+
+  const [docInfo,setDocInfo] = useState([]);
+
+  const fetchDocInfo = async () =>{
+    const docInfo = doctors.find(doc => doc._id === docId)
+    setDocInfo(docInfo);
+  }
+
+  useEffect(()=>{
+    fetchDocInfo();
+  },[doctors,docId])
+  return docInfo && (
     <div>
+      {/* --------- Doctors Details---------- */}
+      <div>
+        <div>
+          <img src={docInfo.image} alt=''/>
+        </div>
+
+        <div>
+          {/* ------------- Doc Info : name, degree, experience ------------- */}
+          <p>
+            {docInfo.name} 
+            <img src={assets.verified_icon} alt=''/>
+          </p>
+        </div>
+
+      </div>
     </div>
   )
 }
