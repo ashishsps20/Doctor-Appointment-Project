0dc7cd0d7bed87c639d1108bb94b0e53bde25cf8
GovindSaini1234a <Govindsaini1234a@gmail.com>
2025-06-10 13:23:19 +0530

    added server and gitignore

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
-import connectDB from './config/mongoose'
+import connectDB from './config/mongoose.js'
 import connectCloudinary from './config/cloudinary.js'
 const app =express();
 const port = process.env.port || 4000
