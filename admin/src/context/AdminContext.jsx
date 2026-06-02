1f47d18725691bb17680ca710ec8148d46023160
ashishsps20 <ashishsps20@gmail.com>
2026-06-02 12:37:31 +0530

    implement dashboard data retrieval and integrate with AdminContext

--- admin/src/context/AdminContext.jsx
+++ admin/src/context/AdminContext.jsx
@@ -6,14 +6,15 @@ export const AdminContext = createContext()
 
 const AdminContextProvider = (props) => {
     const [aToken, setAToken] = useState(localStorage.getItem('aToken') || null);
-    const [doctors,setDoctors] = useState([])
+    const [doctors, setDoctors] = useState([])
     const [appointments, setAppointments] = useState([])
+    const [dashData, setDashData] = useState(false);
 
     const backendUrl = import.meta.env.VITE_BACKEND_URL;
 
     const getAllDoctors = async () => {
         try {
-            const {data} = await axios.post(`${backendUrl}/api/admin/all-doctors`, {},{
+            const { data } = await axios.post(`${backendUrl}/api/admin/all-doctors`, {}, {
                 headers: {
                     Authorization: `Bearer ${aToken}`,
                     atoken: aToken
@@ -34,7 +35,7 @@ const AdminContextProvider = (props) => {
 
     const changeAvailability = async (docId) => {
         try {
-            const {data} = await axios.post(`${backendUrl}/api/admin/change-availability`, {docId}, {       
+            const { data } = await axios.post(`${backendUrl}/api/admin/change-availability`, { docId }, {
                 headers: {
                     Authorization: `Bearer ${aToken}`,
                     atoken: aToken
@@ -48,30 +49,45 @@ const AdminContextProvider = (props) => {
             }
         } catch (error) {
             toast.error(error?.response?.data?.message || 'Failed to change availability')
-        }  
+        }
     }
 
     const getAllAppointments = async () => {
-        try{
-            const {data} = await axios.get(`${backendUrl}/api/admin/appointments`, {headers: {aToken}})
+        try {
+            const { data } = await axios.get(`${backendUrl}/api/admin/appointments`, { headers: { aToken } })
             if (data.success) {
                 setAppointments(data.appointments)
                 console.log(data.appointments)
-            }else{
+            } else {
                 toast.error(data.message)
             }
-        } catch(error){
+        } catch (error) {
             toast.error(error?.response?.data?.message || 'Failed to fetch appointments')
         }
     }
 
     const cancelAppointment = async (appointmentId) => {
         try {
-            const {data} = await axios.post(`${backendUrl}/api/admin/cancel-appointment`, {appointmentId}, {headers: {aToken}})
-            if (data.success){
+            const { data } = await axios.post(`${backendUrl}/api/admin/cancel-appointment`, { appointmentId }, { headers: { aToken } })
+            if (data.success) {
                 toast.success(data.message)
                 getAllAppointments();
-            }else{
+            } else {
+                toast.error(data.message)
+            }
+        } catch (error) {
+            toast.error(error.message || 'Failed to fetch appointments')
+        }
+    }
+
+    const getDashData = async () => {
+
+        try {
+            const { data } = await axios.get(`${backendUrl}/api/admin/dashboard`, { headers: { aToken } })
+            if (data.success) {
+                setDashData(data.dashData)
+                console.log(data.dashData)
+            } else {
                 toast.error(data.message)
             }
         } catch (error) {
@@ -89,7 +105,8 @@ const AdminContextProvider = (props) => {
         appointments,
         getAllAppointments,
         setAppointments,
-        cancelAppointment
+        cancelAppointment,
+        dashData,getDashData
     }
     return (
         <AdminContext.Provider value={value}>
--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -4,6 +4,7 @@ import { v2 as cloudinary } from 'cloudinary'
 import doctorModel from '../models/doctorModel.js'
 import appointmentModel from '../models/appointmentModel.js'
 import jwt from 'jsonwebtoken'
+import userModel from '../models/userModel.js'
 
 
 // api for adding doctor by admin
@@ -141,6 +142,27 @@ export const appointmentCancel = async (req, res) => {
         }
 
         res.json({success: true, message: 'Appointment Cancelled'})
+    } catch (error) {
+        console.log(error);      
+        res.json({ success: false, message: error.message });
+    }
+}
+// API to get dashboard data for admin panel
+export const adminDashboard = async(req,res) => {
+
+    try {
+        const doctors = await doctorModel.find({})
+        const users = await userModel.find({})
+        const appointments = await appointmentModel.find({})
+        const dashData = {
+            doctors: doctors.length,
+            appointments:appointments.length,
+            patients: users.length,
+            latestAppointments: appointments.reverse().slice(0,5)
+        }
+
+        res.json({success:true,dashData})
+
     } catch (error) {
         console.log(error);      
         res.json({ success: false, message: error.message });
